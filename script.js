2e4b4c4b313973c3b47dd205378922b5c2772274
Jordan <jordankuang226@gmail.com>
2024-02-20 12:56:34 -0800

    second commit

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let firstNum = null;
 let secondNum = null;
 let operator = null;
 let displayValue = '';
+let displayValueArray = [];
 
 const zeroButton = document.getElementById('zeroButton');
 const oneButton = document.getElementById('oneButton');
@@ -21,136 +22,68 @@ const clearButton = document.getElementById('clearButton');
 const equalsButton = document.getElementById('equalsButton');
 
 zeroButton.addEventListener('click', () => {
-    if (!(operator === null)) {
-        document.getElementById('display').textContent = '';
-    }
     populateDisplay('0');
-    secondNum = parseInt(displayValue);
 });
 
 oneButton.addEventListener('click', () => {
-    if (!(operator === null)) {
-        document.getElementById('display').textContent = '';
-    }
     populateDisplay('1');
-    secondNum = parseInt(displayValue);
 });
 
 twoButton.addEventListener('click', () => {
-    if (!(operator === null)) {
-        document.getElementById('display').textContent = '';
-    }
     populateDisplay('2');
-    secondNum = parseInt(displayValue);
 });
 
 threeButton.addEventListener('click', () => {
-    if (!(operator === null)) {
-        document.getElementById('display').textContent = '';
-    }
     populateDisplay('3');
-    secondNum = parseInt(displayValue);
 });
 
 fourButton.addEventListener('click', () => {
-    if (!(operator === null)) {
-        document.getElementById('display').textContent = '';
-    }
     populateDisplay('4');
-    secondNum = parseInt(displayValue);
 });
 
 fiveButton.addEventListener('click', () => {
-    if (!(operator === null)) {
-        document.getElementById('display').textContent = '';
-    }
     populateDisplay('5');
-    secondNum = parseInt(displayValue);
 });
 
 sixButton.addEventListener('click', () => {
-    if (!(operator === null)) {
-        document.getElementById('display').textContent = '';
-    }
     populateDisplay('6');
-    secondNum = parseInt(displayValue);
 });
 
 sevenButton.addEventListener('click', () => {
-    if (!(operator === null)) {
-        document.getElementById('display').textContent = '';
-    }
     populateDisplay('7');
-    secondNum = parseInt(displayValue);
 });
 
 eightButton.addEventListener('click', () => {
-    if (!(operator === null)) {
-        document.getElementById('display').textContent = '';
-    }
     populateDisplay('8');
-    secondNum = parseInt(displayValue);
 });
 
 nineButton.addEventListener('click', () => {
-    if (!(operator === null)) {
-        document.getElementById('display').textContent = '';
-    }
     populateDisplay('9');
-    secondNum = parseInt(displayValue);
 });
 
 addButton.addEventListener('click', () => {
     operator = '+';
-
-    if (firstNum === null) {
-        firstNum = parseInt(displayValue);
-    } else {
-        secondNum = parseInt(displayValue);
-    }
-
-    displayValue = '';
+    populateDisplay(' + ');
 });
 
 subtractButton.addEventListener('click', () => {
     operator = '-';
-
-    if (firstNum === null) {
-        firstNum = parseInt(displayValue);
-    } else {
-        secondNum = parseInt(displayValue);
-    }
-
-    displayValue = '';
+    populateDisplay(' - ');
 });
 
 multiplyButton.addEventListener('click', () => {
     operator = '*';
-
-    if (firstNum === null) {
-        firstNum = parseInt(displayValue);
-    } else {
-        secondNum = parseInt(displayValue);
-    }
-
-    displayValue = '';
+    populateDisplay(' * ');
 });
 
 divideButton.addEventListener('click', () => {
     operator = '/';
-
-    if (firstNum === null) {
-        firstNum = parseInt(displayValue);
-    } else {
-        secondNum = parseInt(displayValue);
-    }
-
-    displayValue = '';
+    populateDisplay(' / ');
 });
 
 equalsButton.addEventListener('click', () => {
-    console.log(firstNum, secondNum, operator);
     operate(firstNum, secondNum, operator);
+    console.log(firstNum, secondNum, operator);
 });
 
 clearButton.addEventListener('click', () => {
@@ -179,23 +112,24 @@ const divide = function(num1, num2) {
 }
 
 const operate = function(num1, num2, operator) {
+    displayValueArray = displayValue.split(' ');
     if (operator === '+') {
-        firstNum = num1 + num2;
+        firstNum = parseInt(displayValueArray[displayValueArray.length - 3]) + parseInt(displayValueArray[displayValueArray.length - 1]);
         secondNum = null;
         displayValue = firstNum.toString();
         document.getElementById('display').textContent = displayValue;
     } else if (operator === '-') {
-        firstNum = num1 - num2;
+        firstNum = parseInt(displayValueArray[displayValueArray.length - 3]) - parseInt(displayValueArray[displayValueArray.length - 1]);
         secondNum = null;
         displayValue = firstNum.toString();
         document.getElementById('display').textContent = displayValue;
     } else if (operator === '*') {
-        firstNum = num1 * num2;
+        firstNum = parseInt(displayValueArray[displayValueArray.length - 3]) * parseInt(displayValueArray[displayValueArray.length - 1]);
         secondNum = null;
         displayValue = firstNum.toString();
         document.getElementById('display').textContent = displayValue;
     } else if (operator === '/') {
-        firstNum = num1 / num2;
+        firstNum = parseInt(displayValueArray[displayValueArray.length - 3]) / parseInt(displayValueArray[displayValueArray.length - 1]);
         secondNum = null;
         displayValue = firstNum.toString();
         document.getElementById('display').textContent = displayValue;
